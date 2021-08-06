6d1b32618c8da98f8eb9ccb858785166fc663b3c
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-07 00:32:02 +0530

    initial commit

---
+++ apps/compito/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
