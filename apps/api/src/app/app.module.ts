e4543d49ec98cdd1ce14b71ca0e3e01f041718f5
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-08 00:47:08 +0530

    feat(server): add security and compression middlewares

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { OrganizationModule } from './organization/organization.module';
+import { ProjectModule } from './project/project.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     AuthModule,
     OrganizationModule,
-    // ProjectModule,
+    ProjectModule,
     // TaskModule,
     // TagModule,
     // UserModule,
