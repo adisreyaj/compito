d7c8c49bd53a916d1c0e459fc68f4e7fd35742f1
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-08 00:44:38 +0530

    feat(server): add security and compression middlewares

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { OrganizationModule } from './organization/organization.module';
+import { ProjectModule } from './project/project.module';
 
 @Module({
   imports: [
     AuthModule,
     OrganizationModule,
-    // ProjectModule,
+    ProjectModule,
     // TaskModule,
     // TagModule,
     // UserModule,
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,30 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
 import { AppModule } from './app/app.module';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors({
+    origin: '*',
+  });
+  app.use(compression());
+  app.use(helmet());
+  if (configService.get('NODE_ENV') === 'PRODUCTION') {
+    app.use(
+      rateLimit({
+        windowMs: 1 * 60 * 1000, // 1 minutes
+        max: 60, // limit each IP to 60 requests per windowMs
+      })
+    );
+  }
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
