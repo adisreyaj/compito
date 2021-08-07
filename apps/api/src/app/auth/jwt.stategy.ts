4a0177ae2f2b6fe2ad6a15d1191c0ca8389f5df1
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-07 19:42:23 +0530

    feat(server-auth): add auth0 jwt strategy

---
+++ apps/api/src/app/auth/jwt.stategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import * as dotenv from 'dotenv';
+import { passportJwtSecret } from 'jwks-rsa';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+dotenv.config();
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${process.env.AUTH0_ISSUER_URL}.well-known/jwks.json`,
+      }),
+
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: `${process.env.AUTH0_ISSUER_URL}`,
+      algorithms: ['RS256'],
+    });
+  }
+
+  validate(payload: unknown): unknown {
+    return payload;
+  }
+}
