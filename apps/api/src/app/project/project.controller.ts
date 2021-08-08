fe6ca77d44f1bc32c555ff49601b0b0d61decc21
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-08 14:13:56 +0530

    feat(server-project): add permission guard

--- apps/api/src/app/project/project.controller.ts
+++ apps/api/src/app/project/project.controller.ts
@@ -8,32 +8,47 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { PERMISSIONS } from 'apps/api/src/app/core/config/permissions.config';
+import { Permissions } from 'apps/api/src/app/core/decorators/permissions.decorator';
+import { PermissionsGuard } from 'apps/api/src/app/core/guards/permissions.guard';
 import { ProjectService } from './project.service';
 
 @Controller('projects')
 export class ProjectController {
   constructor(private organizationService: ProjectService) {}
+
+  @UseGuards(PermissionsGuard)
+  @Permissions(PERMISSIONS.project.create)
   @Post()
   create(@Body() organization: ProjectRequest) {
     return this.organizationService.create(organization);
   }
 
+  @UseGuards(PermissionsGuard)
+  @Permissions(PERMISSIONS.project.read)
   @Get()
   findAll(@Query() query: RequestParamsDto) {
     return this.organizationService.findAll(query);
   }
 
+  @UseGuards(PermissionsGuard)
+  @Permissions(PERMISSIONS.project.read)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.organizationService.findOne(id);
   }
 
+  @UseGuards(PermissionsGuard)
+  @Permissions(PERMISSIONS.project.update)
   @Patch(':id')
   update(@Param('id') id: string, @Body() organization: ProjectRequest) {
     return this.organizationService.update(id, organization);
   }
 
+  @UseGuards(PermissionsGuard)
+  @Permissions(PERMISSIONS.project.delete)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.organizationService.remove(id);
