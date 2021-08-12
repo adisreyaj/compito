83efdf3fe24b37dd2f4f92ee0f6c8326a58af623
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-13 01:07:44 +0530

    feat(ui-users): add breadcrumb to user page

--- libs/web/users/src/lib/users.component.ts
+++ libs/web/users/src/lib/users.component.ts
@@ -1,14 +1,16 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Breadcrumb } from '@compito/web/ui';
 import { Store } from '@ngxs/store';
 import { UsersAction } from './state/users.actions';
 
 @Component({
   selector: 'compito-users',
-  template: ` <compito-page-header title="Users"></compito-page-header> `,
+  template: ` <compito-page-header title="Users" [breadcrumbs]="breadcrumbs"></compito-page-header> `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersComponent implements OnInit {
+  breadcrumbs: Breadcrumb[] = [{ label: 'Home', link: '/' }];
   constructor(private store: Store) {}
 
   ngOnInit(): void {
