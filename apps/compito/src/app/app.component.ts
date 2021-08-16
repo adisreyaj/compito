90be0aefd77089a2b6c30fd1eb32eae3ea1e5909
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-16 23:10:18 +0530

    feat: add cors and status api

--- apps/compito/src/app/app.component.ts
+++ apps/compito/src/app/app.component.ts
@@ -1,7 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'compito-root',
   template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private http: HttpClient) {
+    this.http.get(`${environment.api}/ping`).subscribe(console.log);
+  }
+}
