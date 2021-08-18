6bcc355ca7870f834ebb8f76ebae11b8a74a1f63
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-18 21:39:18 +0530

    feat(ui-tasks): add tasks loading card

--- libs/web/tasks/src/lib/state/tasks.state.ts
+++ libs/web/tasks/src/lib/state/tasks.state.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Task } from '@compito/api-interfaces';
+import { DataLoading, DataLoadingState, Task } from '@compito/api-interfaces';
 import { Action, Selector, State, StateContext } from '@ngxs/store';
 import { tap } from 'rxjs/operators';
 import { TasksService } from '../tasks.service';
@@ -7,10 +7,12 @@ import { TasksActions } from './tasks.actions';
 
 export class TasksStateModel {
   public tasks: Task[] = [];
+  public tasksLoading: DataLoading = { type: DataLoadingState.init };
 }
 
 const defaults: TasksStateModel = {
   tasks: [],
+  tasksLoading: { type: DataLoadingState.init },
 };
 
 @State<TasksStateModel>({
@@ -23,16 +25,31 @@ export class TasksState {
   static getMyTasks(state: TasksStateModel) {
     return state.tasks;
   }
+  @Selector()
+  static tasksLoading(state: TasksStateModel) {
+    return state.tasksLoading;
+  }
 
   constructor(private taskService: TasksService) {}
   @Action(TasksActions.GetMyTasks)
   add({ patchState }: StateContext<TasksStateModel>) {
+    patchState({
+      tasksLoading: { type: DataLoadingState.loading },
+    });
     return this.taskService.getMyTasks().pipe(
-      tap(({ payload: tasks }) => {
-        patchState({
-          tasks,
-        });
-      }),
+      tap(
+        ({ payload: tasks }) => {
+          patchState({
+            tasks,
+            tasksLoading: { type: DataLoadingState.success },
+          });
+        },
+        () => {
+          patchState({
+            tasksLoading: { type: DataLoadingState.error },
+          });
+        },
+      ),
     );
   }
 }
