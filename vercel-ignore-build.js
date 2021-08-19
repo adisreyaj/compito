f6f97f04fbe4bed4fd666153c3c1c582242136fc
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-20 00:22:19 +0530

    chore: update vercel ignore build step

--- vercel-ignore-build.js
+++ vercel-ignore-build.js
@@ -1,7 +1,7 @@
 (() => {
   const VERCEL_ENV = process.env.VERCEL_ENV;
   const COMMIT_MSG = process.env.VERCEL_GIT_COMMIT_MESSAGE;
-  if (VERCEL_ENV === 'production' && COMMIT_MSG.includes('ui')) {
+  if (VERCEL_ENV === 'production' && (COMMIT_MSG.includes('ui') || COMMIT_MSG.includes('pull request'))) {
     process.exit(0);
   }
 })();
