33eb5d5ffff81eeb6b502dbfe974e03be5d61323
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-20 15:49:11 +0530

    chore: fix vercel ignore build script

--- vercel-ignore-build.js
+++ vercel-ignore-build.js
@@ -2,6 +2,6 @@
   const VERCEL_ENV = process.env.VERCEL_ENV;
   const COMMIT_MSG = process.env.VERCEL_GIT_COMMIT_MESSAGE;
   if (VERCEL_ENV === 'production' && (COMMIT_MSG.includes('ui') || COMMIT_MSG.includes('pull request'))) {
-    process.exit(0);
+    process.exit(1);
   }
 })();
