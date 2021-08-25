0a7d179c01f6fdf65dbf03b3f0092d51f01d31f6
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-08-25 21:42:31 +0530

    refactor(ui-project): moved template code to html

--- libs/web/projects/src/lib/projects.component.ts
+++ libs/web/projects/src/lib/projects.component.ts
@@ -14,59 +14,7 @@ import { ProjectsAction } from './state/projects.actions';
 import { ProjectsState } from './state/projects.state';
 @Component({
   selector: 'compito-projects',
-  template: ` <compito-page-header title="Projects" [breadcrumbs]="breadcrumbs"> </compito-page-header>
-    <section class="projects__container">
-      <div class="projects__list px-4 md:px-8">
-        <ng-container *ngIf="(usersLoading$ | async)?.type === 'SUCCESS'">
-          <article
-            *permission="'project:create'"
-            (click)="openProjectModal()"
-            class="p-4 cursor-pointer rounded-md border-2 transition-all duration-200 ease-in
-          border-transparent border-dashed bg-gray-100 hover:bg-gray-200 shadow-sm hover:border-primary
-          grid place-items-center"
-            style="min-height: 194px;"
-          >
-            <div class="flex items-center space-x-2 text-gray-500">
-              <div class=" border rounded-md shadow-sm bg-white">
-                <rmx-icon class="w-5 h-5" name="add-line"></rmx-icon>
-              </div>
-              <p class="text-sm">Add New Project</p>
-            </div>
-          </article>
-        </ng-container>
-        <ng-container [ngSwitch]="(uiView$ | async)?.type">
-          <ng-container *ngSwitchCase="'SUCCESS'">
-            <ng-container *ngFor="let project of projects$ | async">
-              <compito-project-card
-                [data]="project"
-                (clicked)="handleProjectCardEvents($event, project)"
-              ></compito-project-card>
-            </ng-container>
-          </ng-container>
-          <ng-container *ngSwitchCase="'LOADING'">
-            <ng-container *ngFor="let org of [1]">
-              <compito-loading-card height="194px">
-                <div class="flex flex-col justify-between h-full">
-                  <header class="flex flex-col space-y-1">
-                    <shimmer height="24px" width="60%" [rounded]="true"></shimmer>
-                    <shimmer height="14px" [rounded]="true"></shimmer>
-                    <shimmer height="14px" width="50%" [rounded]="true"></shimmer>
-                  </header>
-                  <div class="flex items-center -space-x-4">
-                    <shimmer width="40px" height="40px" borderRadius="50%"></shimmer>
-                    <shimmer width="40px" height="40px" borderRadius="50%"></shimmer>
-                    <shimmer width="40px" height="40px" borderRadius="50%"></shimmer>
-                  </div>
-                  <footer class="flex items-center justify-between">
-                    <shimmer height="16px" width="50%" [rounded]="true"></shimmer>
-                  </footer>
-                </div>
-              </compito-loading-card>
-            </ng-container>
-          </ng-container>
-        </ng-container>
-      </div>
-    </section>`,
+  templateUrl: './projects.component.html',
   styles: [
     `
       .projects {
