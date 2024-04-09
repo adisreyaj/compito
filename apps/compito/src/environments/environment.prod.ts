6719f7325407b09d9585ffb93f5aa97a95a3188f
Adithya Sreyaj <adi.sreyaj@gmail.com>
2024-04-09 22:50:25 +0530

    chore: update api endpoints

--- apps/compito/src/environments/environment.prod.ts
+++ apps/compito/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseURL: 'https://compito.adi.so',
-  api: 'https://compito-api.adi.so/api',
+  api: 'https://compito.api.adi.so/api',
   auth: {
     audience: 'compito-api',
     domain: 'compito.us.auth0.com',
